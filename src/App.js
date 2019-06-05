d9c320b232e45369f6d3e26d3e7edd30b1facc1e
HotsumaX <luislaborde@gmail.com>
2019-06-04 21:37:37 -0400

    fixed eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
+const Pet = ({ name, animal, breed }) =>
+  React.createElement('div', {}, [
     React.createElement('h1', {}, name),
     React.createElement('h2', {}, animal),
     React.createElement('h2', {}, breed)
   ]);
-};
 
-const App = () => {
-  return React.createElement('div', { id: 'something-important' }, [
+const App = () =>
+  React.createElement('div', { id: 'something-important' }, [
     React.createElement('h1', {}, 'Adopt Me!'),
     React.createElement(Pet, {
       name: 'luna',
@@ -20,5 +19,4 @@ const App = () => {
       breed: 'havanese1'
     })
   ]);
-};
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
