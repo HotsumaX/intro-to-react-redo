df93598a51d8c155319895933717e24e0fc3f703
HotsumaX <luislaborde@gmail.com>
2019-06-04 20:05:40 -0400

    added eslint and prettier

---
+++ src/App.js
@@ -0,0 +1,24 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', { id: 'something-important' }, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'luna',
+      animal: 'dog',
+      breed: 'havanese'
+    }),
+    React.createElement(Pet, {
+      name: 'cow',
+      animal: 'cow2',
+      breed: 'havanese1'
+    })
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
