8632a8e5956647d28becb813b5f1e923e366c1ca
HotsumaX <luislaborde@gmail.com>
2019-06-05 10:51:16 -0400

    fixed eslint errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-const Pet = ({ name, animal, breed }) =>
-  React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
-  ]);
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
 
-const App = () =>
-  React.createElement('div', { id: 'something-important' }, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'luna',
-      animal: 'dog',
-      breed: 'havanese'
-    }),
-    React.createElement(Pet, {
-      name: 'cow',
-      animal: 'cow2',
-      breed: 'havanese1'
-    })
-  ]);
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const App = () => {
+  return (
+    <div id="something-important">
+      <h1>Adopt Me!</h1>
+      <Pet name="luna" animal="dog" breed="havanese" />
+    </div>
+
+    // React.createElement('div', { id: 'something-important' }, [
+    //   React.createElement('h1', {}, 'Adopt Me!'),
+    //   React.createElement(<Pet name="luna" animal="dog" breed="havanese" />),
+    //   React.createElement(<Pet name="cow" animal="cow2" breed="havanese1" />)
+    // ])
+  );
+};
+
+render(React.createElement(App), document.getElementById('root'));
