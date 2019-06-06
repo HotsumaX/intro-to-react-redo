7efa0791f85b5d1504842c0a8b0492b1ba83321c
HotsumaX <luislaborde@gmail.com>
2019-06-06 16:25:57 -0400

    added useEffect to seach params component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,23 @@
 /* eslint-disable jsx-a11y/label-has-for */
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed('');
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [setBreed, animal, setBreeds]);
 
   return (
     <div className="search-params">
