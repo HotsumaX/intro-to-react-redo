e421e04feb02167de7fae13dfcb406b2de16fb17
HotsumaX <luislaborde@gmail.com>
2019-06-08 22:49:26 -0400

    cleaned up all errors in details.js and app.js

--- src/Details.js
+++ src/Details.js
@@ -1,35 +1,52 @@
+/* eslint-disable no-console */
 import React from 'react';
-import pet from '@frontendmasters/pet'
+import pet from '@frontendmasters/pet';
+
 class Details extends React.Component() {
-
   state = {
-    loading: true,
-  }
-
-
+    loading: true
+  };
 
   componentDidMount = () => {
-    pet.animal(this.props.id)
-      .then(({animal}) =>{
-        this.setState({
-          name:animal.name,
-          animal: animal.type,
-          location:`${animal.contact.address.city},
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city},
           ${animal.contact.address.state}`,
-          description:animal.description,
-          media: animal.photos,
-          breed:animal.breeds.primary,
-          loading: false
-        })
-      }, console.error)
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
   };
-  
 
-  render()
-  return = (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button type="submit">
+            Adopt
+            {name}
+          </button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+//   return = (
+//     <pre>
+//       <code>{JSON.stringify(props, null, 4)}</code>
+//     </pre>
+//   );
+// };
 export default Details;
