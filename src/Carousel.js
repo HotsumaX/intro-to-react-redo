2cdb260c8edec387f02b5f0e7f18a49943b643d3
HotsumaX <luislaborde@gmail.com>
2019-06-11 15:58:45 -0400

    completed the carousel section

--- src/Carousel.js
+++ src/Carousel.js
@@ -6,6 +6,17 @@ class Carousel extends React.Component {
     active: 0
   };
 
+  static getDerivedStateFromProps({ media }) {
+    let photos = ['http://placecorgi.com/600/600'];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+    return { photos };
+  }
+
+  handleIndexClick() {}
+
   render() {
     const { photos, active } = this.state;
 
