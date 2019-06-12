684bc7675dfaa649c4a63af09c8a828751f196c8
HotsumaX <luislaborde@gmail.com>
2019-06-12 12:40:52 -0400

    added in the theme context file and added it to app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { Router, Link } from '@reach/router';
 import Pet from './Pet';
 import SearchParams from './SearchParams';
 import Details from './Details';
+import ThemeContext from './ThemeContext';
 
 const App = () => {
-  const loading = true;
+  const themeHook = useState('darkblue');
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
 
-        {/* {loading ? <h3>...select an animal</h3> : <Pet />} */}
-      </div>
+          {/* {loading ? <h3>...select an animal</h3> : <Pet />} */}
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
 
     // React.createElement('div', { id: 'something-important' }, [
